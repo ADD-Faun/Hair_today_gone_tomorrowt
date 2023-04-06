b409a734c7f99b64647193698c9be87c9b19d07c
ADD-Faun <adammbus@hotmail.com>
2023-04-06 10:53:36 +0000

    model for booking

--- booking/models.py
+++ booking/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class slot(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
+    customer = models.CharField(max_length=50, null=False, blank=False)
+    server = models.CharField(max_length=50, null=False, blank=False)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
         return self.name
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import slot
+from .forms import slotForm
 
 
 def home(request):
+    if request.method == 'POST':
+        form = slotForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = slotForm()
+    context = {
+        'form': form
+    }
 
-    return render(request, 'booking/booking_main.html')
+    return render(request, 'booking/booking_main.html', context)
