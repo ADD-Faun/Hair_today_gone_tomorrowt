a0cd47c8058e70b9d88a79cc90ce2ef269eadf70
ADD-Faun <adammbus@hotmail.com>
2023-04-08 13:28:21 +0000

    add model choices

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from datetime import datetime
+import datetime as dt
+
+
+HOUR_CHOICES = [(dt.time(hour=x), '{:02d}:00'.format(x)) for x in range(8, 19)]
 
 SERVER_CHOICES = (
     ("one", "One"),
@@ -10,10 +15,10 @@ SERVER_CHOICES = (
 
 
 class slot(models.Model):
-    customer = models.CharField(max_length=50, null=False, blank=False)
-    server = models.CharField(max_length=50,choices=SERVER_CHOICES)
-    date = models.DateField(auto_now=False, auto_now_add=False)
-    time = models.TimeField(auto_now=False, auto_now_add=False)
+    customer = models.CharField(max_length=50, null=False, blank=False, default="your name")
+    server = models.CharField(max_length=50, choices=SERVER_CHOICES)
+    date = models.DateField(auto_now=False, auto_now_add=False, default=datetime.now)
+    time = models.TimeField(auto_now=False, auto_now_add=False, choices=HOUR_CHOICES)
 
     def __str__(self):
         return self.customer
